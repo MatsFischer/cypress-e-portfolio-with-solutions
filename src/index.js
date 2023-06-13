aaf473e09c5228bef6861023bf3e556793e3bfdb
mfischer <fischer@econda.de>
2023-06-13 22:56:13 +0200

    updated test page

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function showContent() {
+    document.getElementById('login-form').reset();
+    document.getElementById('login-form').classList.add('hide');
+    document.getElementById('login-success-message').classList.remove('hide');
+    document.getElementById('todo-list').classList.remove('hide');
+    document.getElementById('user-table').classList.remove('hide');
+}
